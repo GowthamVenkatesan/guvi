959216ef2ec7ccff18479086537527381e70e460
Gowtham Venkatesan <32303791+GowthamVenkatesan@users.noreply.github.com>
2019-06-11 14:29:00 +0530

    Delete detective.py

--- detective.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-l = [int(i) for i in input().strip().split(' ')]
-partialSums = []
-for i in range(n):
-    thisSum = 0
-    for j in range(i):
-        thisSum += l[j]
-    partialSums.append(thisSum)
-print(sum(partialSums))
