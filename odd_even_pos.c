c783a1765f9f813cdb9729ea6d32512054b08b1b
Gowtham Venkatesan <32303791+GowthamVenkatesan@users.noreply.github.com>
2019-06-11 12:21:47 +0530

    Add files via upload

---
+++ odd_even_pos.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    int a;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        if(a % 2 == 0 && i % 2 == 1)
+            printf("%d ", a);
+        else if(a % 2 == 1 && i % 2 == 0)
+            printf("%d ", a);
+    }
+
+    return 0;
+}
