ac7c9751029e3e840dd6a0e108ee1d484d86544a
Gowtham Venkatesan <32303791+GowthamVenkatesan@users.noreply.github.com>
2019-06-11 09:39:37 +0530

    Add files via upload

---
+++ even_or_odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double n;
+    if(scanf("%lf", &n) <= 0)    {
+        printf("invalid");
+        return 0;
+    }
+
+    if(floor(n) != n) {
+        printf("Invalid");
+        return 0;
+    }
+    int nAsInt = n;
+    if((nAsInt & 1) == 1)    printf("Odd");
+    else                printf("Even");
+    
+    return 0;
+}
