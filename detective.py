6896ff7a1d3b33034d9723c9fa2ae0862528fcbc
Gowtham Venkatesan <32303791+GowthamVenkatesan@users.noreply.github.com>
2019-06-11 14:29:13 +0530

    Add files via upload

---
+++ detective.py
@@ -0,0 +1,10 @@
+n = int(input())
+l = [int(i) for i in input().strip().split(' ')]
+partialSums = []
+for i in range(n):
+    thisSum = 0
+    for j in range(i):
+        if l[j] < l[i]:
+            thisSum += l[j]
+    partialSums.append(thisSum)
+print(sum(partialSums))
