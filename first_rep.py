b558537d20659d64bce0490d3d765ee92a6362e8
Gowtham Venkatesan <32303791+GowthamVenkatesan@users.noreply.github.com>
2019-06-11 12:17:48 +0530

    Add files via upload

---
+++ first_rep.py
@@ -0,0 +1,15 @@
+from collections import *
+
+n = int(input())
+l = [int(i) for i in input().split(' ')]
+d = defaultdict(int)
+
+found = False
+for i in range(n):
+    d[l[i]] += 1
+    if d[l[i]] == 2:
+        print(l[i])
+        found = True
+
+if not found:
+    print("unique")
